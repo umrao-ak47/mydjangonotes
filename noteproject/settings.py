1f4190a164f73e0a31e01065ba99f006202299c3
Amit Kumar <ap583217@gmail.com>
2020-07-18 15:41:50 +0530

    Added Heroku Python

--- noteproject/settings.py
+++ noteproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -132,3 +133,5 @@ LOGIN_REDIRECT_URL = 'notes:user_home'
 LOGOUT_URL = 'notes:logout'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+django_heroku.settings(locals())
